f4d3c8ece7a5d233da4b514dbbb87caec4fea8b0
lizethcas <lcastil149@gmail.com>
2026-03-21 15:47:25 -0500

    Switch to puppeteer and remove executable path

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(express.json())
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
